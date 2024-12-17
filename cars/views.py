174220fc97b563edf2934ac0b463069326fb242f
haroldoalcobacas <haroldo.alcobacas@gmail.com>
2024-12-17 17:24:43 -0300

    Criação View Teste Cars

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+
+
+def cars_view(request):
+    cars = Car.objects.all()
+
+    return render(
+        request,
+        'cars\cars.html',
+        {'cars': cars}
+    )
