951b7f09e085a718d37dff15d57e952ad3f5f145
haroldoalcobacas <haroldo.alcobacas@gmail.com>
2024-12-23 15:21:36 -0300

    Creatind app users

--- cars/forms.py
+++ cars/forms.py
@@ -6,3 +6,10 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    #funçoes de validação devem ter o clean
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('valor', 'Valor mínimo do carro deve ser de R$20.000')
+        return value
